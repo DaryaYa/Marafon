7008ea2680d60975503696dfffe86c0367fe4c72
Darya <45762789+DaryaYa@users.noreply.github.com>
2019-10-30 17:34:02 +0300

    final fix

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
 $(function(){
 
-$('.reviews-slider').slick({
+  $('.reviews-slider').slick({
     infinite: true,
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  arrows: false,
-  dots: true
-});
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },]
+  });
+
+  $('.btn__menu').on('click', function(){
+    $('.menu__list').slideToggle();
+  });
 
 });
